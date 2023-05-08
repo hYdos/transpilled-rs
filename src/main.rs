6d8f106c802d6255eba366fdd79616591930095d
Hayden Verschelden <haydenv06@gmail.com>
2023-05-09 08:29:25 +1000

    Runs with access violation

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::mem::MaybeUninit;
 use cty::uint32_t;
 use crate::fast_arm_decoder_bindings::{fad_decode, Inst};
 
 mod fast_arm_decoder_bindings;
 
 fn main() {
-    let asm: uint32_t = 0xd10083ff; // sub sp, sp, #32
-    let mut out: Inst;
-    let n = unsafe { fad_decode(asm, 1, &mut out) };
-    println!("Output: {}", n);
+    unsafe {
+        let asm: uint32_t = 0xd10083ff; // sub sp, sp, #32
+        let mut out = MaybeUninit::uninit();
+        let n = fad_decode(asm, 1, out.as_mut_ptr());
+        let inst = out.assume_init();
+        println!("Output: {}", n);
+        println!("Hello, world!");
+    }
 
-    println!("Hello, world!");
 }
