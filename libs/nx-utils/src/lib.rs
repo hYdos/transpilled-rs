f597b29b8c1f82a49b5522c6d1bed007b352b886
Hayden Verschelden <haydenv06@gmail.com>
2023-05-14 19:11:27 +1000

    Last few touches

--- libs/nx-utils/src/lib.rs
+++ libs/nx-utils/src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io::{Cursor, Read, Seek, SeekFrom};
+use std::str::from_utf8;
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+use crate::NroSegmentType::{DATA, RO, TEXT};
+
+pub enum NroSegmentType {
+    TEXT = 0,
+    RO = 1,
+    DATA = 2,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct NroSegment {
+    segment_type: NroSegmentType,
+    offset: usize,
+    size: usize,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl NroSegment {
+    pub fn new(reader: &mut Cursor<Vec<u8>>, segment_type: NroSegmentType) -> NroSegment {
+        NroSegment {
+            segment_type,
+            offset: reader.read_u32::<LittleEndian>().unwrap() as usize,
+            size: reader.read_u32::<LittleEndian>().unwrap() as usize,
+        }
+    }
+}
+
+pub struct SwitchExecutable {
+    program: Vec<u8>,
+    text: Vec<u8>,
+    ro: Vec<u8>,
+    data: Vec<u8>,
+    bss: Vec<u8>,
+}
+
+impl SwitchExecutable {
+    pub fn read_nro(file_bytes: Vec<u8>) -> SwitchExecutable {
+        let mut reader = Cursor::new(file_bytes.clone());
+        reader.seek(SeekFrom::Current(4)).unwrap();
+        reader.seek(SeekFrom::Current(4)).unwrap(); // mod0: u32
+        reader.seek(SeekFrom::Current(8)).unwrap();
+        let mut buf: [u8; 4] = [0, 0, 0, 0];
+        reader.read_exact(&mut buf).unwrap();
+        let magic = from_utf8(&buf).unwrap();
+        assert_eq!(magic, "NRO0");
+        let version = reader.read_u32::<LittleEndian>().unwrap();
+        let header_size = reader.read_u32::<LittleEndian>().unwrap();
+        reader.seek(SeekFrom::Current(4)).unwrap();
+        let sectors = [
+            NroSegment::new(&mut reader, TEXT),
+            NroSegment::new(&mut reader, RO),
+            NroSegment::new(&mut reader, DATA)
+        ];
+        let bss_size = reader.read_u32::<LittleEndian>().unwrap();
+        reader.seek(SeekFrom::Current(4)).unwrap();
+        reader.seek(SeekFrom::Current(0x20)).unwrap(); // Build ID
+        reader.seek(SeekFrom::Current(0x20)).unwrap();
+
+        // TODO: asset processing?
+
+        let text = file_bytes[sectors[TEXT as usize].offset..][..sectors[TEXT as usize].size].to_vec();
+        let ro = file_bytes[sectors[RO as usize].offset..][..sectors[RO as usize].size].to_vec();
+        let data = file_bytes[sectors[DATA as usize].offset..][..sectors[DATA as usize].size].to_vec();
+        let bss = file_bytes[sectors[DATA as usize].offset + sectors[DATA as usize].size..][..bss_size as usize].to_vec();
+
+        return SwitchExecutable {
+            program: file_bytes.clone(),
+            text,
+            ro,
+            data,
+            bss,
+        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 pub fn main() {
     println!("Hell.")
 }
+
+#[cfg(test)]
+mod tests {
+    use nx_utils::SwitchExecutable;
+    use super::*;
+
+    #[test]
+    fn read_nro_file() {
+        let bytes = include_bytes!("../test/hello-world.nro").to_vec();
+        let nro = SwitchExecutable::read_nro(bytes);
+        println!("e {}", 1);
+    }
+}
